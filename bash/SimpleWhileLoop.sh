f746f50a0732f544f89ce915c133b03cad197f04
Mithun Cheriyath <mcheriyath@users.noreply.github.com>
2018-09-27 14:05:13 -0400

    Update SimpleWhileLoop.sh

--- bash/SimpleWhileLoop.sh
+++ bash/SimpleWhileLoop.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# METHOD 1
+# Given number of retries
 n=0
 until [ $n -ge 3 ]
 do
   wget -O - -q -t 1 http://localhost:8080/ && break
   n=$[$n+1]
   sleep 10
+done
+
+# METHOD 2
+# Retries until it works
+until docker ps
+do
+ sleep 3
 done
