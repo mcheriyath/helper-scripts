712f6f87b4e4c82da16d4070592ef8f1841cd889
Mithun Cheriyath <mcheriyath@users.noreply.github.com>
2020-07-29 10:41:49 -0400

    Update save_sqs_to_file.py

--- aws/sqs/save_sqs_to_file.py
+++ aws/sqs/save_sqs_to_file.py
@@ -12,7 +12,41 @@ import docopt
 
 
 def get_messages_from_queue(queue_url):
-    ...
+"""Generates messages from an SQS queue.
+
+    Note: this continues to generate messages until the queue is empty.
+    Every message on the queue will be deleted.
+
+    :param queue_url: URL of the SQS queue to drain.
+
+    """
+    sqs_client = boto3.client('sqs')
+
+    while True:
+        resp = sqs_client.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=['All'],
+            MaxNumberOfMessages=10
+        )
+
+        try:
+            yield from resp['Messages']
+        except KeyError:
+            return
+
+        entries = [
+            {'Id': msg['MessageId'], 'ReceiptHandle': msg['ReceiptHandle']}
+            for msg in resp['Messages']
+        ]
+
+        resp = sqs_client.delete_message_batch(
+            QueueUrl=queue_url, Entries=entries
+        )
+
+        if len(resp['Successful']) != len(entries):
+            raise RuntimeError(
+                f"Failed to delete messages: entries={entries!r} resp={resp!r}"
+            )
 
 
 if __name__ == '__main__':
